56112692e7e94701f359c48715df7051a8474f3c
gkourouma <gkourouma2@gmail.com>
2025-03-10 18:30:49 -0500

    wip: complete sign in and sign out routers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,63 @@
 const express = require("express");
 const router = express.Router();
-
-module.exports = router;
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
 router.get("/sign-up", (req, res) => {
-    res.render("auth/sign-up.ejs");
-  });
-  
+  res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+    return res.send("Username already taken.");
+  }
+  //check if password and comfirm password is a match
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+  }
+  //create encrypted version of password
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  const user = await User.create(req.body);
+  res.send(`Thanks for signing up ${user.username}`);
+
+  // res.render("sign-up.ejs");
+});
+
+//GET sign in page. Send a page that has a login form
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
+//POST routes that will be used when login form in submitted.
+router.post("/sign-in", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id
+  };
+
+  res.redirect("/");
+
+});
+
+router.get("/sign-out", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+module.exports = router;
